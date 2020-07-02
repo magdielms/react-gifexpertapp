0d674113792a578dac54630b32f0e91ab33d4d1e
Magdiel Matamoros <MagdielMatamoros@hotmail.com>
2020-07-02 18:37:18 -0500

    Docs Agregado

---
+++ docs/precache-manifest.661211b14a367c564e0b3a2bd24fa7f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ee23787b5e93713c343c89ec1ad415",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42a2dde7a7f85eb9689b",
+    "url": "/static/css/main.4aab30b8.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a2dde7a7f85eb9689b",
+    "url": "/static/js/main.5911cbcf.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
